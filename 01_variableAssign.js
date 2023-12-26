97b74b6edf8aa98be20c4ad5f92fa04ee7b65d85
rupalipatil250896 <rupalipatil250896@gmail.com>
2023-12-26 13:30:52 +0530

    created JS variable
    created JS variable

---
+++ 01_variableAssign.js
@@ -0,0 +1,23 @@
+var myFullName;
+console.log(myFullName);
+myFullName= "Rupali  Chaudhari"
+console.log("My First and Last Name:",myFullName);
+myFullName="Rupali Dinesh Chaudhari"
+console.log("My Full Name:",myFullName);
+var myLove;
+myLove="I love only JS"
+console.log(myLove);
+var firstName="Rupali";
+var lastName="Chaudhari";
+var age=27;
+var address="Waghod";
+var pinCode=425508;
+var gender="Female";
+var isMarried="Yes";
+console.log("First Name:",firstName);
+console.log("Last Name:",lastName);
+console.log("Age:",age);
+console.log("Address:",address);
+console.log("Pin Code:",pinCode);
+console.log("Gender:",gender);
+console.log("Is Married:",isMarried);
---
+++ helloWord.js
@@ -0,0 +1,10 @@
+console.log("Hello JS, I am rupali");
+var num;
+console. log(num)
+console.log(10)
+console.log("Rupali");
+var myName="rupali"
+console.log(myName);
+console.log("first name",myName);
+myName="Rupali Chaudhari";
+console.log("Full Name",myName);
